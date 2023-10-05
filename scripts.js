6038e54d190e2d2d36eb398fa591e76539dccdd7
zacharyyi <145707584+zacharyyi@users.noreply.github.com>
2023-10-05 19:21:02 +0000

    Pen Project People

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Get Stuff
+const thumbnail = document.querySelector(".thumbnail");
+const dialog = document.querySelector("dialog");
+
+// Do Stuff
+thumbnail.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialog.addEventListener("click", () => {
+  dialog.close();
+});
